285b832501ce269a85b7145c85d1362c83c7ce1c
bervr <bervrr@gmail.com>
2022-12-25 17:09:46 +0400

    done

--- framework/templator.py
+++ framework/templator.py
@@ -1,11 +1,23 @@
-from jinja2 import Template
+import os
 
-def render(template, **kwargs):
-    with open(template, encoding='utf-8') as file:
-        template = Template(file.read())
+from jinja2 import FileSystemLoader
+from jinja2 import Environment
+
+folder = 'templates'
+root_dir = os.getcwd()
+templates_folder = os.path.join(root_dir, folder)
+
+
+def render(template_name, folder=templates_folder, **kwargs):
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+
+    template = env.get_template(template_name)
+    # with open(template, encoding='utf-8') as file:
+    #     template = Template(file.read())
     return template.render(**kwargs)
 
 
 if __name__ == "__main__":
-    test = render('home_site.html', params=[{'username': 'Alex'}, {'city': 'Moscow'}])
+    test = render('home_site.html') # , params=[{'username': 'Alex'}, {'city': 'Moscow'}])
     print(test)
